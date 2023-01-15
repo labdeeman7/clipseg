907ba4e84e8e2a3f129a6841bc852f6183b788ad
Oluwatosin Alabi <labdeeman77@gmail.com>
2023-01-15 17:39:15 +0000

    maybe memory, at this point, this is trial and error

--- train_clipseg_phrasecut_create.sh
+++ train_clipseg_phrasecut_create.sh
@@ -2,7 +2,6 @@
 #SBATCH --output=/scratch/users/%u/output_terminal/clipseg_%j.out
 #SBATCH --job-name=clipseg_phrasecut
 #SBATCH --gres=gpu
-#SBATCH --mem=10000
 #SBATCH --mail-user=oluwatosin.alabi@kcl.ac.uk
 
 source /users/${USER}/.bashrc
